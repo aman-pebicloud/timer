0c7904df6e09ad39e344508917740332e98e3d9e
Aman <amanullahm@wavity.com>
2017-09-08 17:10:23 +0530

    second

--- src/timer/SchedulerService.java
+++ src/timer/SchedulerService.java
@@ -16,31 +16,34 @@ import org.quartz.impl.StdSchedulerFactory;
 
 public class SchedulerService {
 
-	public <T extends Job> void run(Class<T> myjob) throws SchedulerException, InterruptedException {
+	public <T extends Job> void run(Class<T> myjob, String jobName, String group) throws SchedulerException, InterruptedException {
 		Logger logger = Logger.getAnonymousLogger();
 		Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
 		scheduler.start();
 		logger.info("scheduler started");
-
-		JobDetail jobDetail = newJob(myjob).withIdentity("job1", "group1").build();
-
+		
 		simpleSchedule().withIntervalInSeconds(2);
 		Trigger trigger = newTrigger().withIdentity("trigger1", "group1").startNow()
 				.withSchedule(SimpleScheduleBuilder.repeatSecondlyForTotalCount(10)).build();
 
 		logger.info("trigger initialised, will end at");
-		scheduler.scheduleJob(jobDetail, trigger);
+		scheduler.scheduleJob(createJobDetail(jobName, group, myjob), trigger);
 		;
 		System.out.println("main exits");
 		logger.info("thread sleeeps");
-		Thread.sleep(20000);
+		Thread.sleep(10000);
 		scheduler.shutdown();
 		logger.warning("scheduler stops");
 
 	}
 
 	public static void main(String[] args) throws SchedulerException, InterruptedException {
-		new SchedulerService().run(AnotherJob.class);
-		new SchedulerService().run(MyJob.class);
+		new SchedulerService().run(AnotherJob.class, "job1", "group1");
+		new SchedulerService().run(MyJob.class, "job2", "group1");
+	}
+
+	private <T extends Job> JobDetail createJobDetail(String jobName, String group, Class<T> myjob) {
+		JobDetail jobDetail = newJob(myjob).withIdentity(jobName, group).build();
+		return jobDetail;
 	}
 }
